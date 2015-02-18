1f7c3dfd54a80ba6083663ccccb7b535f9453b9b
Quinn Beightol <qeb@cornell.edu>
2015-02-18 14:37:30 -0500

    finished commonwords

---
+++ a3/html/commonwords.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+cat superbowl.txt | tr '[A-Z] ' '[a-z]\n' > pass1.txt
+grep '[^[:space:]]\+' pass1.txt > pass2.txt
+
+cat pass2.txt | sort | uniq -c | sort -rn | head -100 
+
+# clean up 
+rm pass1.txt
+rm pass2.txt
+
+

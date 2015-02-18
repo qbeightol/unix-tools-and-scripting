142f7fc48b312daa35ac6436cc44b781983eeb53
Quinn Beightol <qeb@cornell.edu>
2015-02-18 14:37:18 -0500

    finished webcrawler

--- a3/html/webcrawler.sh
+++ a3/html/webcrawler.sh
@@ -3,5 +3,4 @@
 wget http://www.cs.cornell.edu/courses/cs2043/2015sp/assignments/superbowl.html
 
 # remove html tags: 
-# should we use [^>]+ or [^>]*
-sed 's/<[^>]\+>'
+sed 's/<[^>]*>//g' superbowl.html > superbowl.txt 

a43fe4813df1d35a3d956a7bd9276d5d9eb53ad8
Quinn Beightol <qeb@cornell.edu>
2015-02-18 19:35:35 -0500

    finished html scraping

--- a3/html/commonwords.sh
+++ a3/html/commonwords.sh
@@ -3,7 +3,7 @@
 cat superbowl.txt | tr '[A-Z] ' '[a-z]\n' > pass1.txt
 grep '[^[:space:]]\+' pass1.txt > pass2.txt
 
-cat pass2.txt | sort | uniq -c | sort -rn | head -100 
+cat pass2.txt | sort | uniq -c | sort -rn | head -100 > commonwords.txt
 
 # clean up 
 rm pass1.txt

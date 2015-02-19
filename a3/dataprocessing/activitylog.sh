afddca1d25fd0040382bca77ed9cd870545f9692
seungheehan4 <sh969@cornell.edu>
2015-02-18 22:21:44 -0500

    finished data processing

--- a3/dataprocessing/activitylog.sh
+++ a3/dataprocessing/activitylog.sh
@@ -1,7 +1,16 @@
-start=0
-awk ' BEGIN{FS = ",|:|"}
-	NR > 1 {if $2 = "start" {$start=$4 + ($5/60)}
-            if $2 = "end" {Activity[$3]+= ($4 + $5/60)-$start}
-           }
-    END {for (a in Activity) print a ": " Activity[a] " hours"
-    ' activity_log.csv
+gawk ' BEGIN{FS = ",|:"}
+       NR > 1 {if ($2 == "start work") 
+                  Activity["work"]-= $3;
+               else if ($2 == "end work")
+                  Activity["work"]+= $3;
+               if ($2 == "start run") 
+                  Activity["run"]-= $3;
+               else if ($2 == "end run")
+                  Activity["run"]+= $3;
+               if ($2 == "start farmers market") 
+                  Activity["farmers market"]-= $3;
+               else if ($2 == "end farmers market")
+                  Activity["farmers market"]+= $3;
+              }
+       END {for (a in Activity) print a ": " Activity[a] " hours"}
+     ' activity_log.csv

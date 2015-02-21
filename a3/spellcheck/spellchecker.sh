6f3aded57b6347252d37e9394c03f915abd80b8c
Quinn Beightol <qeb@cornell.edu>
2015-02-20 20:03:11 -0500

    tweaked spellchecker so it uses grep's -i flag

--- a3/spellcheck/spellchecker.sh
+++ a3/spellcheck/spellchecker.sh
@@ -1,13 +1,9 @@
 #! /bin/bash
 
 cat $1 | tr -d '[:punct:]' | sed 's/[[:space:]]\+/\n/g' > pass1.txt
-cat pass1.txt | tr '[A-Z]' '[a-z]' > pass2.txt
 
-cat english.txt | tr '[A-Z]' '[a-z]' > decap_english.txt
-
-grep -F -v -x -f decap_english.txt pass2.txt > spellchecker.txt
+grep -F -v -x -i -f english.txt pass1.txt > spellchecker.txt
 
 # clean up
 rm pass1.txt
-rm pass2.txt
-rm decap_english.txt
+

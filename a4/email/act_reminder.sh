e970977508cb97af8df0d461d5fee8b9f8604225
seungheehan4 <sh969@cornell.edu>
2015-02-28 16:48:00 -0500

    done

--- a4/email/act_reminder.sh
+++ a4/email/act_reminder.sh
@@ -1,6 +1,3 @@
 #! /bin/bash
-day=date | cut -f 1 -d " " 
-
-list=gawk ' BEGIN NR>1 {if ($1 == $day) $list=$3;} END' myweekly_act.txt
-
-echo $list
+today=`date | gawk '{print $1}'`
+gawk -v today=$today '{FS = " : "}{if($1==today) print $2}' myweekly_act.txt | mailx -s "reminder!" qeb2@cornell.edu

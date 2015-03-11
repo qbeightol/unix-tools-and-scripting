13ca2eed1c54542adb53890d64fa76c6343b5bfe
seungheehan4 <sh969@cornell.edu>
2015-03-11 04:29:10 -0400

    done

---
+++ a5/themovies.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/bash
+
+
+while true
+do
+url="https://web.archive.org/web/20140301052344/http://www.movies.com/rss-feeds/top-ten-box-office-rss"
+title_html='<title><\!\[CDATA\[[0-9]\{1,2\}.*</title>'
+description_html='<description>.*</description>'
+curl $url 2> /dev/null | grep -o $title_html | sed "s/<title><!\[CDATA\[//g" | sed "s/\]\]><\/title>//g"
+synopsis=$(curl $url 2> /dev/null | grep -o $description_html | sed "s/<description><!\[CDATA\[//g" | sed "s/\]\]><\/description>/\n/g")
+IFS=$'\n'
+array=($synopsis)
+read -p "Choose a movie (1-10) > " mov_num
+  while true
+  do 
+  printf "Movie $mov_num\nSynopsis\n\n"
+  echo ${array[$mov_num]}
+  read -p "Please enter to return"
+  break;
+  done
+done

57a0b223b71650831c271f7a1fdfdd6f87b870d7
seungheehan4 <sh969@cornell.edu>
2015-03-12 17:13:54 -0400

    DONE DONE DONE

--- a5/themovies.sh
+++ a5/themovies.sh
@@ -7,7 +7,7 @@ url="https://web.archive.org/web/20140301052344/http://www.movies.com/rss-feeds/
 title_html='<title><\!\[CDATA\[[0-9]\{1,2\}.*</title>'
 description_html='<description>.*</description>'
 curl $url 2> /dev/null | grep -o $title_html | sed "s/<title><!\[CDATA\[//g" | sed "s/\]\]><\/title>//g"
-synopsis=$(curl $url 2> /dev/null | grep -o $description_html | sed "s/<description><!\[CDATA\[//g" | sed "s/\]\]><\/description>/\n/g")
+synopsis=$(curl $url 2> /dev/null | grep -o $description_html | sed "s/<br.*>//g" | sed "s/<description><!\[CDATA\[//g" | sed "s/\]\]><\/description>//g")
 IFS=$'\n'
 array=($synopsis)
 read -p "Choose a movie (1-10) > " mov_num
